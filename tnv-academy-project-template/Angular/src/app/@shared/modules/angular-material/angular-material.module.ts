f8b22eb76fd293e01c4b868f51c8bd490cdc50f5
izzystitt <isabella.fip98@gmail.com>
2023-04-18 18:44:21 +0200

    alcune modifiche profilo e movie selection

--- tnv-academy-project-template/Angular/src/app/@shared/modules/angular-material/angular-material.module.ts
+++ tnv-academy-project-template/Angular/src/app/@shared/modules/angular-material/angular-material.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
-  exports: [MatButtonModule, MatCardModule, MatIconModule],
+  exports: [MatButtonModule, MatCardModule, MatIconModule, MatSidenavModule ],
 })
 export class AngularMaterialModule {}
