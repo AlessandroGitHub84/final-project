ab0cf9196f7fc45203e0bfc5a7a8e522a63e341d
izzystitt <isabella.fip98@gmail.com>
2023-04-18 11:59:28 +0200

    aggiunta pagina movie-selection

---
+++ tnv-academy-project-template/Angular/src/app/components/movie-selection/movie-selection.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tnv-movie-selection',
+  templateUrl: './movie-selection.component.html',
+  styleUrls: ['./movie-selection.component.scss']
+})
+export class MovieSelectionComponent {
+
+}
