3b60575570fd4431ac66a9c9c7e5fbb0582d9d8f
izzystitt <isabella.fip98@gmail.com>
2023-04-19 11:43:26 +0200

    sistemazione progetto

---
+++ tnv-academy-project-template/Node/app/src/models/rating.js
@@ -0,0 +1,20 @@
+import { Sequelize } from "sequelize"; 
+import db from "../../config/config.js";
+ 
+const { DataTypes } = Sequelize;
+
+const Rating = db.define('ratings', {
+  userId: {
+    type: DataTypes.STRING
+  },
+  movieId: {
+    type: DataTypes.STRING
+  },
+  rating: {
+    type: DataTypes.INTEGER
+  }
+}, {
+  freezeTableName: true
+});
+ 
+export default Rating;
